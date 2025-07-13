be4624b88a1c83317a519b995f9a6d9447b478e8
Logan Foster <44828794+Lrfoster03@users.noreply.github.com>
2025-07-12 18:23:11 -0700

    Fixed directed edge resizing and infill bug with loops (#9)
    Fixed directed edge resizing and infill bug with loops

--- src/Edge.js
+++ src/Edge.js
@@ -9,25 +9,38 @@ function Edge(props) {
     const [x2, y2] = [end.position[0] - offsetLeft, end.position[1]]     //Ending Vertex
     const color = edge.color
     const directedBool = edge.directedBool
+    const markerId = `arrow-${color.replace('#', '')}`;
 
-
-    let d;
+    let d, markerWidth, markerHeight, refX, refY;
     if (isLoop) {
         const radius = 35 + (multiplicity * 10);
-        const far = Math.sqrt(2) * radius
+        const far = Math.sqrt(2) * radius;
         d = `M ${x1} ${y1}
-             a ${radius},${radius} 0 0 1 ${-far},${-far}
-             a ${radius},${radius} 0 1 1 ${far},${far}`
-    } else {
-        const magnitude = Math.sqrt(Math.pow(x1-x2, 2) + Math.pow(y1-y2, 2))
-        const [vx, vy] = [(y2-y1) / magnitude, -(x2-x1) / magnitude]
-        const distance = 25 * Math.ceil(multiplicity / 2)
-        const direction = Math.pow(-1, multiplicity) * (vx >= vy ? (vx === vy ? (x1 > x2 ? 1 : -1) : 1) : -1)
-        const [midX, midY] = [(x1 + x2)/2, (y1 + y2)/2]
-        const [x3, y3] = [midX + (distance * direction * vx), midY + (distance * direction * vy)]
+            a ${radius},${radius} 0 0 1 ${-far},${-far}
+            a ${radius},${radius} 0 1 1 ${far},${far}`;
 
+        // Set default marker size for loops
+        markerWidth = 5.5;
+        markerHeight = 3.85;
+        refX = 4.5;
+        refY = 1.925;
+        
+    } else {
+        const magnitude = Math.sqrt(Math.pow(x1-x2, 2) + Math.pow(y1-y2, 2));
+        const [vx, vy] = [(y2-y1) / magnitude, -(x2-x1) / magnitude];
+        const distance = 25 * Math.ceil(multiplicity / 2);
+        const direction = Math.pow(-1, multiplicity) * (vx >= vy ? (vx === vy ? (x1 > x2 ? 1 : -1) : 1) : -1);
+        const [midX, midY] = [(x1 + x2)/2, (y1 + y2)/2];
+        const [x3, y3] = [midX + (distance * direction * vx), midY + (distance * direction * vy)];
+        
         d = `M ${x1} ${y1}
-             Q ${x3} ${y3} ${x2} ${y2}`
+            Q ${x3} ${y3} ${x2} ${y2}`;
+        
+        const arrowScale = Math.max(5, Math.min(magnitude * 0.0075, 10));
+        markerWidth = arrowScale;
+        markerHeight = arrowScale * 0.7;
+        refX = arrowScale - 0.5;
+        refY = markerHeight / 2;
     }
 
 
@@ -35,18 +48,31 @@ function Edge(props) {
     {
         return (
             <svg>
-                <marker id="arrow" markerWidth="10" markerHeight="7" refX="9.5" refY="2.625" orient="auto">
-                        <polygon onMouseDown={onMouseDown} stroke={color} strokeWidth='0' fill={color} points='0 0, 9.5 2.7, 0 5.25' />
+                <marker
+                    id={markerId}
+                    markerWidth={markerWidth}
+                    markerHeight={markerHeight}
+                    refX={refX}
+                    refY={refY}
+                    orient="auto"
+                >
+                    <polygon
+                        onMouseDown={onMouseDown}
+                        stroke={color}
+                        strokeWidth='0'
+                        fill={color}
+                        points={`0 0, ${refX} ${refY}, 0 ${markerHeight}`}
+                    />
                 </marker>
                 <path
-                    marker-end='url(#arrow)'
+                    markerEnd={`url(#${markerId})`}
                     onMouseDown={onMouseDown}
                     className='Edge-Path'
-                    fill={color}
+                    fill= "none"
                     stroke={color}
                     strokeWidth='5'
                     d={d}
-                    />
+                />
             </svg>
         )
     }
@@ -57,7 +83,7 @@ function Edge(props) {
                 onMouseDown={onMouseDown}
                 className='Edge-Path'
                 fill='transparent'
-                stroke={color}
+                stroke={edge.color}
                 strokeWidth='5'
                 d={d}
             />
