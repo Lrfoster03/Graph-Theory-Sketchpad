53b7ec487a5f52a94f49750727cd1cdfdd1637d8
Logan Foster <44828794+Lrfoster03@users.noreply.github.com>
2025-07-12 18:20:10 -0700

    Allow users to export and import nodes and vertices off sketchpad(#8)
    Allow users to export and import nodes and vertices off sketchpad using JSON

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
+// ...existing code...
 import React, {useState} from 'react';
+
 import './App.css';
 import Field from './Field';
 import SideBar from "./SideBar";
 import ClickAction from "./ClickAction";
 
-
 function App() {
   const [clickAction, setClickAction] = useState(ClickAction.SELECT)
   const [color, setColor] = useState('#000000');
+  const [vertices, setVertices] = useState([]);
+  const [edges, setEdges] = useState([]);
 
   return (
     <div className="App">
@@ -16,10 +19,18 @@ function App() {
         setClickAction={setClickAction}
         color={color}
         setColor={setColor}
+        vertices={vertices}
+        edges={edges}
+        setVertices={setVertices}
+        setEdges={setEdges}
       />
       <Field
         clickAction={clickAction}
         color={color}
+        vertices={vertices}
+        setVertices={setVertices}
+        edges={edges}
+        setEdges={setEdges}
       />
     </div>
   );
